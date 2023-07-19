2ad015bdff3760dc761a03e990666ba51907ad6d
imad <imad.oulasri@iu-study.org>
2023-07-19 18:08:18 +0100

    shell sort

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -36,21 +36,3 @@ void shell_sort(int *array, size_t size)
         print_array(array, size);
     }
 }
-
-/**
- * main - Entry point
- *
- * Return: Always 0
- */
-int main(void)
-{
-    int array[] = {19, 48, 99, 71, 13, 52, 96, 73, 86, 7};
-    size_t n = sizeof(array) / sizeof(array[0]);
-
-    print_array(array, n);
-    printf("\n");
-    shell_sort(array, n);
-    printf("\n");
-    print_array(array, n);
-    return (0);
-}
