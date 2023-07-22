77b3ff981eb7398dc233af7f3e455962e7532b65
imad <imad.oulasri@iu-study.org>
2023-07-22 12:27:47 +0100

    insertion sort updated

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -9,31 +9,31 @@
 
 void insertion_sort_list(listint_t **list)
 {
-    listint_t *j, *temp, *curr;
+	listint_t *j, *temp, *curr;
 
-    if (!list || !*list || !(*list)->next)
-        return;
+	if (!list || !*list || !(*list)->next)
+		return;
 
-    curr = (*list)->next;
-    while (curr)
-    {
-        j = curr;
-        while (j->prev && j->n < j->prev->n)
-        {
-            temp = j->prev;
-            if (temp->prev)
-                temp->prev->next = j;
-            j->prev = temp->prev;
-            temp->prev = j;
-            temp->next = j->next;
-            if (j->next)
-                j->next->prev = temp;
-            j->next = temp;
+	curr = (*list)->next;
+	while (curr)
+	{
+		j = curr;
+		while (j->prev && j->n < j->prev->n)
+		{
+			temp = j->prev;
+			if (temp->prev)
+				temp->prev->next = j;
+			j->prev = temp->prev;
+			temp->prev = j;
+			temp->next = j->next;
+			if (j->next)
+				j->next->prev = temp;
+			j->next = temp;
 
-            if (!j->prev)
-                *list = j;
-            print_list(*list);
-        }
-        curr = curr->next;
-    }
+			if (!j->prev)
+				*list = j;
+			print_list(*list);
+		}
+		curr = curr->next;
+	}
 }
